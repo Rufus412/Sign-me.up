9544d9af58f291e9fd48c3de541464e49bec7faf
Rufus412 <jacob@sannes.se>
2023-07-13 08:31:40 +0200

    text eddit

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -107,7 +107,7 @@ export default {
 
 
             <div class="sm:col-span-3">
-              <label for="postal-code" class="block text-sm font-medium leading-6 text-gray-900">ZIP / Postal code</label>
+              <label for="postal-code" class="block text-sm font-medium leading-6 text-gray-900">Postal code</label>
               <div class="mt-0">
                 <input type="text" name="postal-code" id="postal-code" v-model="info.postalCode" autocomplete="postal-code" class="block w-[99%] rounded-md border py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
               </div>
