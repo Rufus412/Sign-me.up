812b09f87bbc08f934b78683b63d71804fe4b580
Rufus412 <jacob@sannes.se>
2023-07-17 15:20:06 +0200

    Super simple phonenumber verification

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -72,7 +72,7 @@ export default {
 
 <template>
   <div class="">
-    <form @submit.prevent="onSubmit" novalidate> 
+    <form @submit.prevent="onSubmit" > 
       <div class="space-y-12">
 
         <div class="border-b border-gray-900/10 pb-12 ">
@@ -142,7 +142,7 @@ export default {
             <div class="sm:col-span-5">
               <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
               <div class="mt-0">
-                <input id="email" required pattern="[a-zA-Z0-9\._\%\+\-]+@[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,}$" name="email" type="email" v-model="info.eMail" placeholder="Email" autocomplete="email" class="block w-[99%] rounded-md border py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 peer" :class="{ 'invalid:[&:not(:focus):invalid]:border-red-500': failedSubmit }"/>
+                <input id="email" required name="email" type="email" v-model="info.eMail" placeholder="Email" autocomplete="email" class="block w-[99%] rounded-md border py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 peer" :class="{ 'invalid:[&:not(:focus):invalid]:border-red-500': failedSubmit }"/>
                 <span class="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block" :class="{ 'peer-[&:not(:focus):invalid]:block': failedSubmit }"  >
                   Enter a valid Email!
               </span>
@@ -152,7 +152,7 @@ export default {
             <div class="sm:col-span-full">
               <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Phone Number</label>
               <div class="mt-0">
-                <input  id="country" pattern="\S+.*" required name="country" v-model="info.phoneNumber" placeholder="+1 (555) 987-6543" class="block w-[99%] rounded-md border py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 peer" :class="{ 'invalid:[&:not(:focus):invalid]:border-red-500': failedSubmit }"/>
+                <input  id="country" pattern="\+[0-9]{5,}$" required name="country" v-model="info.phoneNumber" placeholder="+1 (555) 987-6543" class="block w-[99%] rounded-md border py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 peer" :class="{ 'invalid:[&:not(:focus):invalid]:border-red-500': failedSubmit }"/>
                 <span class="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block" :class="{ 'peer-[&:not(:focus):invalid]:block': failedSubmit }"  >
                 Enter a valid value!
               </span>
