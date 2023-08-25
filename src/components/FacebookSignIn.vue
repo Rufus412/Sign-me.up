1bdbb1b49d8c63f46841605894a26b520c217290
Rufus412 <jacob@sannes.se>
2023-08-25 08:25:44 +0200

    Cleanup

--- src/components/FacebookSignIn.vue
+++ src/components/FacebookSignIn.vue
@@ -9,30 +9,30 @@ import { useStore } from '../stores/counter';
 <script>
 
 export default {
-    methods: {
-        checkLoginState() {
-            FB.login((response) => {
-            
-            if (response.authResponse) {
-                console.log('Welcome!  Fetching your information.... ');
-                //console.log(response); // dump complete info
-                console.log(response.authResponse)
-                const store = useStore()
-                
-                console.log("This is the UID " + response.authResponse.userID)
-                store.memberID = response.authResponse.userID
+  methods: {
+    checkLoginState() {
+      FB.login((response) => {
 
-                
-                apiService.getName()
-                this.$router.push( {name: 'formCheck' })
-            } else {
-                //user hit cancel button
-                console.log('User cancelled login or dd not fully authorize.');
+        if (response.authResponse) {
+          console.log('Welcome!  Fetching your information.... ');
+          //console.log(response); // dump complete info
+          console.log(response.authResponse)
+          const store = useStore()
 
-            }
-            }, { scope: 'public_profile,email'});
-        },
-    }
+          console.log("This is the UID " + response.authResponse.userID)
+          store.memberID = response.authResponse.userID
+
+
+          apiService.getName()
+          this.$router.push({ name: 'formCheck' })
+        } else {
+          //user hit cancel button
+          console.log('User cancelled login or dd not fully authorize.');
+
+        }
+      }, { scope: 'public_profile,email' });
+    },
+  }
 }
 </script>
 
@@ -41,22 +41,21 @@ export default {
 
 
 <template>
-
-    <button type="button" @click="checkLoginState" class="loginBtn loginBtn--facebook flex border-0 w-full justify-center cursor-pointer rounded-md  px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm hover:bg-[#458FEF] ">
-        <span class="text-white" id="fbText">{{ $t('frontPage.facebookButton') }}</span>
-    </button>
-
+  <button type="button" @click="checkLoginState"
+    class="loginBtn loginBtn--facebook flex border-0 w-full justify-center cursor-pointer rounded-md  px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm hover:bg-[#458FEF] ">
+    <span class="text-white" id="fbText">{{ $t('frontPage.facebookButton') }}</span>
+  </button>
 </template>
 
 
 
 
 <style scoped>
-
 .loginBtn {
   box-sizing: border-box;
   position: relative;
 }
+
 .loginBtn:before {
   content: "";
   box-sizing: border-box;
@@ -75,10 +74,12 @@ export default {
   /*font-family: "Helvetica neue", Helvetica Neue, Helvetica, Arial, sans-serif;*/
   text-shadow: 0 -1px 0 #354C8C;
 }
+
 .loginBtn--facebook:before {
   border-right: #364e92 1px solid;
   background: url('https://s3-us-west-2.amazonaws.com/s.cdpn.io/14082/icon_facebook.png') 6px 6px no-repeat;
 }
+
 .loginBtn--facebook:focus {
   background-color: #5B7BD5;
   background-image: linear-gradient(#5B7BD5, #4864B1);
