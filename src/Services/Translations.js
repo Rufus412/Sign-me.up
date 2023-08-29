e81884630e917c27cbf2ee7b7d0c8c7748185a0d
Rufus412 <jacob@sannes.se>
2023-08-29 13:20:01 +0200

    Added function

--- src/Services/Translations.js
+++ src/Services/Translations.js
@@ -1,3 +1,8 @@
+export async function setPersistedLocale(locale) {
+    localStorage.setItem("user-locale", locale);
+    
+}
+
 export async function loadConfigFiles(fileName) {
     const response = await fetch(`${import.meta.env.VITE_DEFAULT_URL}/config/${fileName}.json`);
     return response.json();
